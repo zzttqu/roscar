43e51792e29c5981939d05978d5cda1e8aeb4bb9
zzttqu <1161085395@qq.com>
2023-03-04 22:41:33 +0800

    改为接受float类型STM32数据

--- src/serial_demo/src/myData.h
+++ src/serial_demo/src/myData.h
@@ -1,12 +1,17 @@
+typedef union _uart_Float_
+{
+  float f_data;
+  uint8_t byte[4];
+} uart_Float;
 typedef struct _Data_Transer_
 {
     unsigned char buffer[64];
     struct _Data_
     {
         unsigned char Data_Header;
-        float X_speed;
-        float Y_speed;
-        float Z_speed;
+        uart_Float X_speed;
+        uart_Float Y_speed;
+        uart_Float Z_speed;
         unsigned char Data_Tail;
     } data;
 } Data_Transer;
@@ -18,9 +23,10 @@ typedef struct _AGV_Vel_
     float Z;
 }AGV_Vel;
 
+//这个是在世界坐标系下的坐标，不是车自身坐标系下的
 typedef struct _AGV_Pos_
 {
     float X;
     float Y;
-    float Z;
+    float Yaw;
 }AGV_Pos;
--- src/serial_demo/src/serial_demo.cpp
+++ src/serial_demo/src/serial_demo.cpp
@@ -11,6 +11,7 @@ using namespace std;
 
 #define HEADER 'S'
 #define TAIL 'E'
+#define PI 3.14159
 
 // 发送和接受串口消息
 char serialReqData[64];
@@ -26,6 +27,7 @@ private:
     serial::Timeout to = serial::Timeout::simpleTimeout(100);
     AGV_Vel agv_vel;
     AGV_Pos agv_pos;
+    float posx = 0, posy = 0, dx = 0, dy = 0, dz = 0;
 
 public:
     // 串口初始化
@@ -62,15 +64,23 @@ public:
         }
     }
 
-    float Odom_Trans(uint8_t Data_High, uint8_t Data_Low)
+    void Odom_Trans(uint8_t data[])
     {
-        float data_return;
-        short transition_16;
-        transition_16 = 0;
-        transition_16 |= Data_High << 8;
-        transition_16 |= Data_Low;
-        data_return = (transition_16 / 1000) + (transition_16 % 1000) * 0.001; // 转化为float类型
-        return data_return;
+        for (size_t i = 2; i < 14; i++)
+        {
+            if (i < 6)
+            {
+                serial_Res_Data.data.X_speed.byte[(i - 2) % 4] = serial_Res_Data.buffer[i];
+            }
+            else if (5 < i && i < 10)
+            {
+                serial_Res_Data.data.Y_speed.byte[(i - 2) % 4] = serial_Res_Data.buffer[i];
+            }
+            else if (9 < i && i < 14)
+            {
+                serial_Res_Data.data.Z_speed.byte[(i - 2) % 4] = serial_Res_Data.buffer[i];
+            }
+        }
     }
 
     bool Get_Data()
@@ -87,13 +97,13 @@ public:
         if (count == 8) // 验证数据包的长度
         {
             serial_Res_Data.data.Data_Header = serial_Res_Data.buffer[0];
-            serial_Res_Data.data.Data_Tail = serial_Res_Data.buffer[7];
+            serial_Res_Data.data.Data_Tail = serial_Res_Data.buffer[14];
             count = 0;                                  // 为串口数据重新填入数组做准备
             if (serial_Res_Data.data.Data_Tail == TAIL) // 验证数据包的帧尾
             {
-                agv_vel.X = Odom_Trans(serial_Res_Data.buffer[1], serial_Res_Data.buffer[2]); // 获取运动底盘X方向速度
-                agv_vel.Y = Odom_Trans(serial_Res_Data.buffer[3], serial_Res_Data.buffer[4]); // 获取运动底盘Y方向速度
-                agv_vel.Z = Odom_Trans(serial_Res_Data.buffer[5], serial_Res_Data.buffer[6]); // 获取运动底盘Z方向速度
+                agv_vel.X = serial_Res_Data.data.X_speed.f_data/1000; // 获取运动底盘X方向速度,并除以1000换算为m/s
+                agv_vel.Y = serial_Res_Data.data.Y_speed.f_data/1000;// 获取运动底盘Y方向速度
+                agv_vel.Z = serial_Res_Data.data.Z_speed.f_data/1000; // 获取运动底盘Z方向速度
                 return true;
             }
         }
@@ -108,55 +118,51 @@ public:
         geometry_msgs::TransformStamped odom_trans;
         ros::NodeHandle n;
         static ros::Publisher pub_odom = n.advertise<nav_msgs::Odometry>("/odom", 1000);
-        // 把Z轴转角转换为四元数进行表达
-        geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(agv_pos.Z);
         nav_msgs::Odometry odom; // 实例化里程计话题数据
-        ros::Time current_time=ros::Time::now();
+
+        ros::Time current_time = ros::Time::now();
+
+        double dt = 0.05; // 循环获取STM32速度
+        // 这里是AGV本身坐标系下的
+        dx = agv_vel.X * dt;
+        dy = agv_vel.Y * dt;
+        dz = agv_vel.Z * dt;
+        // 转化到世界坐标系下
+        agv_pos.Yaw += dz;
+        // 根据角度简化防止超过2pai
+        agv_pos.Yaw = (agv_pos.Yaw > PI) ? (agv_pos.Yaw - 2 * PI) : ((agv_pos.Yaw < -PI) ? (agv_pos.Yaw + 2 * PI) : agv_pos.Yaw);
+        // 世界坐标系下
+        agv_pos.X -= dx * cos(agv_pos.Yaw) + dy * sin(agv_pos.Yaw);
+        agv_pos.Y += dx * sin(agv_pos.Yaw) - dy * cos(agv_pos.Yaw);
+        // 把Z轴转角转换为四元数进行表达
+        geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(agv_pos.Yaw);
+
         // 发布坐标变换父子坐标系
         odom_trans.header.stamp = current_time;
         odom_trans.header.frame_id = "odom";
         odom_trans.child_frame_id = "base_link";
         // 填充获取的数据
-        odom_trans.transform.translation.x = posy; // x坐标
-        odom_trans.transform.translation.y = posx; // y坐标
-        odom_trans.transform.translation.z = 0;    // z坐标
-        odom_trans.transform.rotation = odom_quat; // 偏航角
+        odom_trans.transform.translation.x = agv_pos.X; // x坐标
+        odom_trans.transform.translation.y = agv_pos.Y; // y坐标
+        odom_trans.transform.translation.z = 0;         // z坐标
+        odom_trans.transform.rotation = odom_quat;      // 偏航角
         // 发布tf坐标变换
         odom_broadcaster.sendTransform(odom_trans);
 
-        odom.header.stamp = ros::Time::now();
+        odom.header.stamp = current_time;
         odom.header.frame_id = "odom";         // 里程计TF父坐标
         odom.pose.pose.position.x = agv_pos.X; // 位置
         odom.pose.pose.position.y = agv_pos.Y;
         odom.pose.pose.position.z = 0;
         odom.pose.pose.orientation = odom_quat; // 姿态，通过Z轴转角转换的四元数
-        odom.child_frame_id = "base_footprint"; // 里程计TF子坐标
+
+        odom.child_frame_id = "base_link";      // 里程计TF子坐标
         odom.twist.twist.linear.x = agv_vel.X;  // X方向速度
         odom.twist.twist.linear.y = agv_vel.Y;  // Y方向速度
         odom.twist.twist.angular.z = agv_vel.Z; // 绕Z轴角速度
 
         pub_odom.publish(odom); // Pub odometer topic //发布里程计话题
-    }
-
-    void Control()
-    {
-        static ros::Time _Last_Time = ros::Time::now();
-        while (ros::ok())
-        {
-            ros::Time _Now = ros::Time::now();
-            double Sampling_Time = (_Now - _Last_Time).toSec(); // 获取时间间隔，用于积分速度获得位移(里程)
-            if (true == Get_Data())                             // 通过串口读取并校验下位机发送过来的数据，然后数据转换为国际单位
-            {
-                agv_pos.X += (agv_vel.X * cos(agv_pos.Z) - agv_vel.Y * sin(agv_pos.Z)) * Sampling_Time; // 计算X方向的位移，单位：m
-                agv_pos.Y += (agv_vel.X * sin(agv_pos.Z) + agv_vel.Y * cos(agv_pos.Z)) * Sampling_Time; // 计算Y方向的位移，单位：m
-                agv_pos.Z += agv_vel.Z * Sampling_Time;                                                 // 绕Z轴的角位移，单位：rad
-                Publish_Odom();                                                                         // 发布里程计话题
-
-                _Last_Time = _Now; // 记录时间，用于计算时间间隔
-            }
-
-            ros::spinOnce(); // 循环等待回调函数
-        }
+        ros::spinOnce();
     }
 };
 
