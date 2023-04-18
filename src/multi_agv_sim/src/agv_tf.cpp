c83cff9080b8203577f6aff8298e91be5f5b88a4
zzttqu <1161085395@qq.com>
2023-04-18 20:15:57 +0800

    实现了2车追1车，2车用1车的map

--- src/multi_agv_sim/src/agv_tf.cpp
+++ src/multi_agv_sim/src/agv_tf.cpp
@@ -2,37 +2,46 @@
 #include <tf2_ros/transform_broadcaster.h>
 #include "geometry_msgs/TransformStamped.h"
 #include "tf2/LinearMath/Quaternion.h"
+#include <nav_msgs/Odometry.h>
+//负责将agv1转化到agv0的地图下
+void agv_pos_tf(const nav_msgs::Odometry::ConstPtr &msg)
+{
+    static tf2_ros::TransformBroadcaster broadcaster;
+    geometry_msgs::TransformStamped tfs;
+    // tfs.header.seq=100;
+    tfs.header.frame_id = "agv_0/map";
+    tfs.header.stamp = ros::Time::now();
+
+    tfs.child_frame_id = "agv_1/odom";
+    tfs.transform.translation.x = msg->pose.pose.position.x;
+    tfs.transform.translation.y = msg->pose.pose.position.y;
+    tfs.transform.translation.z = msg->pose.pose.position.x;
+
+    tf2::Quaternion qtn;
+    qtn.setRPY(msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z);
+    tfs.transform.rotation.x = qtn.getX();
+    tfs.transform.rotation.y = qtn.getY();
+    tfs.transform.rotation.z = qtn.getZ();
+    tfs.transform.rotation.w = qtn.getW();
+    broadcaster.sendTransform(tfs);
+    // ROS_ERROR_STREAM(tfs.transform);
+}
+
 int main(int argc, char *argv[])
 {
     setlocale(LC_ALL, "");
     ros::init(argc, argv, "agv_tf");
-    ROS_INFO("坐标变换已启动");
+    ROS_ERROR("坐标变换已启动");
     ros::NodeHandle n;
-    tf2_ros::TransformBroadcaster broadcaster;
-    ros::Rate rate(50);
+
+    ros::Rate rate(10);
     float y = 0.25;
-    while (ros::ok())
-    {
-        
-        geometry_msgs::TransformStamped tfs;
-        // tfs.header.seq=100;
-        tfs.header.frame_id = "base_link";
-        tfs.header.stamp = ros::Time::now();
+    ros::Subscriber sub = n.subscribe<nav_msgs::Odometry>("agv_1/odom",1000, agv_pos_tf);
+    // while (ros::ok())
+    // {
 
-        tfs.child_frame_id = "multi_center";
-        tfs.transform.translation.x = 0.0;
-        tfs.transform.translation.y = y;
-        tfs.transform.translation.z = 0.0;
-
-        tf2::Quaternion qtn;
-        qtn.setRPY(0, 0, 0);
-        tfs.transform.rotation.x = qtn.getX();
-        tfs.transform.rotation.y = qtn.getY();
-        tfs.transform.rotation.z = qtn.getZ();
-        tfs.transform.rotation.w = qtn.getW();
-        broadcaster.sendTransform(tfs);
-        rate.sleep();
-    }
-
+    //     rate.sleep();
+    // }
+    ros::spin();
     return 0;
 }
--- src/multi_agv_sim/src/tf_listener.cpp
+++ src/multi_agv_sim/src/tf_listener.cpp
@@ -2,28 +2,44 @@
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2_ros/transform_listener.h>
 #include "geometry_msgs/TransformStamped.h"
+#include "geometry_msgs/Twist.h"
 #include "tf2/LinearMath/Quaternion.h"
 int main(int argc, char *argv[])
 {
     setlocale(LC_ALL, "");
     ros::init(argc, argv, "tf_listener");
-    ROS_INFO("坐标变换已启动");
+    ROS_ERROR("坐标变换已启动");
     ros::NodeHandle n;
+    ros::Publisher agv_1_vel = n.advertise<geometry_msgs::Twist>("agv_1/cmd_vel", 10);
     tf2_ros::TransformBroadcaster broadcaster;
     tf2_ros::Buffer buffer;
     tf2_ros::TransformListener listener(buffer);
-    ros::Rate rate(50);
-    float y = 0.25;
+    ros::Rate rate(5);
     while (ros::ok())
     {
-        
-        geometry_msgs::TransformStamped tf1;
-        try{
-           geometry_msgs::TransformStamped tf2;
-           tf2=buffer.lookupTransform("agv0/map","agv1/map",ros::Time(0));
+
+        try
+        {
+            geometry_msgs::TransformStamped tf2;
+            tf2 = buffer.lookupTransform("agv_1/base_link", "agv_0/base_link", ros::Time(0));
+            geometry_msgs::Twist vel_msg;
+            double angular = 4*atan2(tf2.transform.translation.y, tf2.transform.translation.x);
+            double vel = 0.5*sqrt(pow(tf2.transform.translation.x, 2) + pow(tf2.transform.translation.y, 2));
+            if (vel < 1)
+            {
+                vel = 0;
+                angular = 0;
+            }
+
+            ROS_WARN_STREAM(tf2.transform);
+            ROS_WARN_STREAM(angular << " " << vel);
+            vel_msg.angular.z = angular;
+            vel_msg.linear.x = vel;
+            agv_1_vel.publish(vel_msg);
         }
-        catch(const std::exception& r){
-            ROS_INFO_STREAM(r.what());
+        catch (const std::exception &r)
+        {
+            ROS_ERROR_STREAM(r.what());
         }
         rate.sleep();
     }
