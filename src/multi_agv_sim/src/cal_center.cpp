4388e463a44d7a4b610a71b840b701c7a55c1d31
zzttqu <zzttqu@gmail.com>
2023-04-21 01:58:35 +0800

    理论上已经完成了，两车朝中心点集合，然后对接

--- src/multi_agv_sim/src/cal_center.cpp
+++ src/multi_agv_sim/src/cal_center.cpp
@@ -1,6 +1,66 @@
 #include <ros/ros.h>
 #include <tf2_ros/transform_broadcaster.h>
+#include <std_msgs/Int32.h>
 #include <tf2_ros/transform_listener.h>
+using namespace std;
+// 将收到的状态放入数组中
+int agvs_status[32];
+// 只有agv_Status为2倍才是全部锁定了
+int agv_Status;
+stringstream ss;
+void agv_status_callback(const std_msgs::Int32::ConstPtr &status, int i)
+{
+    agv_Status = 0;
+    agvs_status[i] = status->data;
+    for (auto x : agvs_status)
+    {
+        agv_Status += x;
+    }
+}
+
+void center_cal(string agv_link_names[],
+                int num,
+                geometry_msgs::TransformStamped map_agv_pos[],
+                geometry_msgs::TransformStamped &center,
+                tf2_ros::TransformBroadcaster &broadcaster,
+                tf2_ros::Buffer &buffer)
+{
+    double x, y, yaw;
+    try
+    {
+        for (size_t i = 0; i < num; i++)
+        {
+            map_agv_pos[i] = buffer.lookupTransform("agv_0/map", agv_link_names[i], ros::Time(0));
+            x += map_agv_pos[i].transform.translation.x;
+            y += map_agv_pos[i].transform.translation.y;
+            yaw += map_agv_pos[i].transform.rotation.z;
+        }
+        tf2::Quaternion qtn;
+        qtn.setRPY(0, 0, yaw / num);
+        center.header.frame_id = "agv_0/map";
+        center.header.stamp = ros::Time::now();
+        center.child_frame_id = "agv_ass/base_link";
+        center.header.seq = 10;
+        center.transform.rotation.w = qtn.getW();
+        center.transform.rotation.x = qtn.getX();
+        center.transform.rotation.y = qtn.getY();
+        center.transform.rotation.z = qtn.getZ();
+        center.transform.translation.x = x / num;
+        center.transform.translation.y = y / num;
+        center.transform.translation.z = 0;
+        broadcaster.sendTransform(center);
+        ROS_WARN("中心坐标计算成功");
+        x = 0;
+        y = 0;
+        yaw = 0;
+    }
+    catch (const std::exception &e)
+    {
+        ROS_ERROR_STREAM("中心坐标计算失败" << e.what() << '\n');
+        buffer.clear();
+        ros::Duration(0.5).sleep();
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -8,61 +68,48 @@ int main(int argc, char *argv[])
     ros::init(argc, argv, "cal_center");
     ROS_WARN("agv中心计算节点已启动");
     ros::NodeHandle n;
-    tf2_ros::TransformBroadcaster broadcaster;
-    tf2_ros::Buffer buffer;
-    tf2_ros::TransformListener listener(buffer);
     int num = 2;
     n.getParam("num", num);
     geometry_msgs::TransformStamped map_agv_pos[num];
+    ros::Subscriber agvs_status_sub[num];
     geometry_msgs::TransformStamped center;
+    tf2_ros::TransformBroadcaster broadcaster;
+    tf2_ros::Buffer buffer;
+    // 不listen会直接导致超时！！！！
+    tf2_ros::TransformListener listener(buffer);
     ros::Rate rate(10);
-    char agv_name[num][32];
+    string agv_link_names[num];
+    string agv_move_names[num];
     for (size_t i = 0; i < num; i++)
     {
-        sprintf(agv_name[i], "agv_%ld/base_link", i);
+        ss << "agv_" << i << "/base_link";
+        agv_link_names[i] = ss.str();
+        ss.str(string());
+        ss << "agv_" << i << "/move_status";
+        agvs_status_sub[i] = n.subscribe<std_msgs::Int32>(ss.str(), 10, boost::bind(agv_status_callback, _1, i));
+        ss.str(string());
     }
-    double x, y, yaw;
+
     // ROS_WARN("准备进入循环");
-    // 以10Hz发布n辆车中点
+    // 计算各车中点
+    ros::Duration(0.5).sleep();
+    center_cal(agv_link_names, num, map_agv_pos, center, broadcaster, buffer);
     while (ros::ok())
     {
+        // 之后虽然不计算了但是还要发布，记得更新时间戳
+        center.header.stamp = ros::Time::now();
+        broadcaster.sendTransform(center);
+
+        // 全部锁定后就可以启动中心导航了
+        if (agv_Status == (2 * num))
+        {
+            // 这里应该加一个允许接收目标点的flag，但是现在不需要
+        }
 
         // 查询各AGV相对绝对地图的位置
-        try
-        {
-            for (size_t i = 0; i < num; i++)
-            {
-                map_agv_pos[i] = buffer.lookupTransform("agv_0/map", agv_name[i], ros::Time(0), ros::Duration(0.1));
-                x += map_agv_pos[i].transform.translation.x;
-                y += map_agv_pos[i].transform.translation.y;
-                yaw += map_agv_pos[i].transform.rotation.z;
-            }
-            tf2::Quaternion qtn;
-            qtn.setRPY(0, 0, yaw / num);
-            center.header.frame_id = "agv_0/map";
-            center.header.stamp = ros::Time::now();
-            center.child_frame_id = "agv_ass/base_link";
-            center.transform.rotation.w = qtn.getW();
-            center.transform.rotation.x = qtn.getX();
-            center.transform.rotation.y = qtn.getY();
-            center.transform.rotation.z = qtn.getZ();
-            center.transform.translation.x = x / num;
-            center.transform.translation.y = y / num;
-            center.transform.translation.z = 0;
-            broadcaster.sendTransform(center);
-            // ROS_WARN("中心坐标更新成功");
-            x = 0;
-            y = 0;
-            yaw = 0;
-        }
-        catch (const std::exception &e)
-        {
-            ROS_INFO_STREAM(e.what() << '\n');
-            buffer.clear();
-        }
 
         rate.sleep();
     }
-
+    ros::spin();
     return 0;
 }
--- src/multi_agv_sim/src/tf_listener.cpp
+++ src/multi_agv_sim/src/tf_listener.cpp
@@ -3,157 +3,162 @@
 #include <tf2_ros/transform_listener.h>
 #include "geometry_msgs/TransformStamped.h"
 #include "geometry_msgs/Twist.h"
+#include <std_msgs/Int32.h>
 #include "tf2/LinearMath/Quaternion.h"
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
 #include "actionlib/client/simple_goal_state.h"
 using namespace actionlib;
-void subscribe(const geometry_msgs::Twist::ConstPtr& agv0_vel)
+using namespace std;
+
+void subscribe(const geometry_msgs::Twist::ConstPtr &agv0_vel)
 {
     agv0_vel->angular.x;
     agv0_vel->angular.z;
 }
-// 0是未抵达状态，1是已经抵达，-1是无法抵达
+// 0是未抵达状态，1是已经抵达，-1是无法抵达，2是已经锁定，订阅center并使用坐标变换
 int assamble_status = 0;
+
+void nav_to_pos(
+    string ns,
+    SimpleActionClient<move_base_msgs::MoveBaseAction> &MoveBaseClient,
+    move_base_msgs::MoveBaseGoal &goal,
+    tf2_ros::Buffer &buffer, string goal_link)
+{
+    geometry_msgs::TransformStamped nav_goal_tf = buffer.lookupTransform("agv_0/map", goal_link, ros::Time(0));
+    goal.target_pose.pose.position.x = nav_goal_tf.transform.translation.x;
+    goal.target_pose.pose.position.y = nav_goal_tf.transform.translation.y;
+    goal.target_pose.pose.orientation.z = nav_goal_tf.transform.rotation.z;
+    goal.target_pose.pose.orientation.w = nav_goal_tf.transform.rotation.w;
+    goal.target_pose.header.stamp = ros::Time::now();
+    MoveBaseClient.sendGoal(goal);
+    // 5s后如果没获得结果就算超时
+    MoveBaseClient.waitForResult(ros::Duration(5));
+    if (MoveBaseClient.getState() == SimpleClientGoalState::SUCCEEDED)
+    {
+        ROS_INFO_STREAM(ns << "已到达预定位置");
+        assamble_status = 1;
+    }
+    else if (SimpleClientGoalState::ACTIVE)
+    {
+        ROS_INFO_STREAM(ns << "正在前往预定位置");
+        assamble_status = 0;
+    }
+    else if (SimpleClientGoalState::PENDING)
+    {
+        MoveBaseClient.sendGoal(goal);
+        ROS_WARN_STREAM(ns << "未收到坐标信息，已重新发送");
+    }
+    else
+    {
+        assamble_status = -1;
+        ROS_ERROR_STREAM(ns << "无法到达预定位置");
+    }
+}
+
 int main(int argc, char *argv[])
 {
     setlocale(LC_ALL, "");
     ros::init(argc, argv, "tf_listener");
-    ROS_ERROR("agv2控制节点已启动");
+    ROS_ERROR("agv控制节点已启动");
     ros::NodeHandle n;
-    ros::Publisher agv_1_vel = n.advertise<geometry_msgs::Twist>("agv_1/cmd_vel", 1000);
-    // ros::Subscriber agv_0_vel = n.subscribe<geometry_msgs::Twist>("agv_0/cmd_vel", 1000, subscribe);
-
-    SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient("agv_1/move_base", true);
+    // 发布当前AGV状态
+    std_msgs::Int32 topic_msg;
     move_base_msgs::MoveBaseGoal goal;
+
+    ros::Publisher move_status = n.advertise<std_msgs::Int32>("move_status", 10);
+    std::string ns = n.getNamespace();
+    // 去除第一位的/防止出错
+    ns = ns.erase(0, 1);
+    ros::Publisher agv_vel = n.advertise<geometry_msgs::Twist>("cmd_vel", 100);
+    // ros::Subscriber agv_0_vel = n.subscribe<geometry_msgs::Twist>("agv_0/cmd_vel", 1000, subscribe);
+    SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient("move_base", true);
 
     tf2_ros::TransformBroadcaster broadcaster;
     tf2_ros::Buffer buffer;
     tf2_ros::TransformListener listener(buffer);
     ros::Rate rate(5);
     ros::Duration(1).sleep();
-    ROS_ERROR("agv2控制节点已正式启动");
-    goal.target_pose.header.frame_id = "map";
+    ROS_ERROR("agv控制节点已正式启动");
+    goal.target_pose.header.frame_id = "agv_0/map";
+    geometry_msgs::TransformStamped nav_goal_tf;
+
+    stringstream ass_pos;
+    ass_pos << "agv_ass/base_link/" << ns;
+    stringstream agv_pos;
+    agv_pos << ns << "/base_link";
+
+    int ass_pos_count = 0;
     try
     {
-        // 初始状态下先过去
-        geometry_msgs::TransformStamped map_asspos = buffer.lookupTransform("map", "agv_1/ass_pos", ros::Time(0));
-        goal.target_pose.pose.position.x = map_asspos.transform.translation.x;
-        goal.target_pose.pose.position.y = map_asspos.transform.translation.y;
-        goal.target_pose.pose.orientation.z = map_asspos.transform.rotation.z;
-        goal.target_pose.pose.orientation.w = map_asspos.transform.rotation.w;
-        goal.target_pose.header.stamp = ros::Time::now();
-
-        MoveBaseClient.sendGoal(goal);
-        // 30s后如果没获得结果就算超时
-        MoveBaseClient.waitForResult(ros::Duration(5));
-        if (MoveBaseClient.getState() == SimpleClientGoalState::SUCCEEDED)
-        {
-            ROS_INFO("AGV2已到达预定位置");
-            assamble_status = 1;
-        }
-        else if (SimpleClientGoalState::ACTIVE)
-        {
-            ROS_INFO("AGV2正在前往预定位置");
-            assamble_status = 0;
-        }
-        else if (SimpleClientGoalState::PENDING)
-        {
-            MoveBaseClient.sendGoal(goal);
-            ROS_WARN("AGV2未收到坐标信息，已重新发送");
-        }
-        else
-        {
-            assamble_status = -1;
-            ROS_ERROR("AGV2无法到达预定位置");
-        }
+        nav_to_pos(ns, MoveBaseClient, goal, buffer, ass_pos.str());
     }
     catch (const std::exception &e)
     {
-        ROS_ERROR_STREAM(e.what());
+        ROS_ERROR_STREAM("导航点发布失败" << e.what() << '\n');
+        ros::Duration(0.5).sleep();
     }
+
+    topic_msg.data = assamble_status;
+    move_status.publish(topic_msg);
     ROS_ERROR("准备进入循环");
     while (ros::ok())
     {
-        try
+
+        // 未设定目标
+        if (assamble_status == -1)
         {
-            // 未设定目标
-            if (assamble_status == -1)
+            try
             {
-                geometry_msgs::TransformStamped map_asspos = buffer.lookupTransform("map", "agv_1/ass_pos", ros::Time(0));
-                goal.target_pose.pose.position.x = map_asspos.transform.translation.x;
-                goal.target_pose.pose.position.y = map_asspos.transform.translation.y;
-                goal.target_pose.pose.orientation.z = map_asspos.transform.rotation.z;
-                goal.target_pose.pose.orientation.w = map_asspos.transform.rotation.w;
-                goal.target_pose.header.stamp = ros::Time::now();
-                // ROS_WARN_STREAM(tf2.transform);
-                // ROS_WARN_STREAM(angular << " " << vel);
+                nav_to_pos(ns, MoveBaseClient, goal, buffer, ass_pos.str());
+            }
+            catch (const std::exception &e)
+            {
+                ROS_ERROR_STREAM("导航点发布失败" << e.what() << '\n');
+                ros::Duration(0.5).sleep();
+            }
 
+            topic_msg.data = assamble_status;
+            move_status.publish(topic_msg);
+        }
+        // 正在前往
+        if (assamble_status == 0)
+        {
+            // MoveBaseClient.waitForResult(ros::Duration(10));
+            if (MoveBaseClient.getState() == SimpleClientGoalState::SUCCEEDED)
+            {
+                ROS_INFO_STREAM(ns << "已到达预定位置");
+                assamble_status = 1;
+            }
+            else if (SimpleClientGoalState::ACTIVE)
+            {
+                ROS_INFO_STREAM(ns << "正在前往预定位置");
+                assamble_status = 0;
+            }
+            else if (SimpleClientGoalState::PENDING)
+            {
                 MoveBaseClient.sendGoal(goal);
-
-                MoveBaseClient.waitForResult(ros::Duration(10));
-
-                if (MoveBaseClient.getState() == SimpleClientGoalState::SUCCEEDED)
-                {
-                    ROS_INFO("AGV2已到达预定位置");
-                    assamble_status = 1;
-                }
-                else if (SimpleClientGoalState::ACTIVE)
-                {
-                    ROS_WARN("AGV2还未抵达预定位置");
-                    assamble_status = 0;
-                }
-                else if (SimpleClientGoalState::PENDING)
-                {
-                    MoveBaseClient.sendGoal(goal);
-                    ROS_WARN("AGV2未收到坐标信息，已重新发送");
-                }
-                else
-                {
-                    ROS_ERROR("AGV2无法到达预定位置");
-                    assamble_status = -1;
-                }
+                ROS_WARN_STREAM(ns << "未收到坐标信息，已重新发送");
             }
-            // 正在前往
-            if (assamble_status == 0)
+            else
             {
-                geometry_msgs::TransformStamped map_asspos = buffer.lookupTransform("map", "agv_1/ass_pos", ros::Time(0));
-                // 这里可以判断一下距离还有多远，或者目标点是否有变化
-                //  double a[3];
-                //  if((goal.target_pose.pose.position.x-map_asspos.transform.translation.x)>0.1){
-
-                // }
-                goal.target_pose.pose.position.x = map_asspos.transform.translation.x;
-                goal.target_pose.pose.position.y = map_asspos.transform.translation.y;
-                goal.target_pose.pose.orientation.z = map_asspos.transform.rotation.z;
-                goal.target_pose.pose.orientation.w = map_asspos.transform.rotation.w;
-                goal.target_pose.header.stamp = ros::Time::now();
-                // MoveBaseClient.waitForResult(ros::Duration(10));
-                SimpleClientGoalState Goalstate = MoveBaseClient.getState();
-                if (MoveBaseClient.getState() == SimpleClientGoalState::SUCCEEDED)
-                {
-                    ROS_INFO("AGV2已到达预定位置");
-                    assamble_status = 1;
-                }
-                else if (SimpleClientGoalState::ACTIVE)
-                {
-                    ROS_WARN("AGV2正在前往预定位置");
-                    assamble_status = 0;
-                }
-                else
-                {
-                    ROS_ERROR("AGV2无法到达预定位置");
-                    assamble_status = -1;
-                }
+                assamble_status = -1;
+                ROS_ERROR_STREAM(ns << "无法到达预定位置");
             }
+            topic_msg.data = assamble_status;
+            move_status.publish(topic_msg);
+        }
 
-            // 进入锁定状态
-            if (assamble_status == 1)
+        // 进入锁定状态
+        // 迅速对齐，稳定后在判定为锁定成功
+        if (assamble_status == 1 && ass_pos_count < 3)
+        {
+            try
             {
-                geometry_msgs::TransformStamped agv2_asspos = buffer.lookupTransform("agv_1/base_link", "agv_1/ass_pos", ros::Time(0));
+                geometry_msgs::TransformStamped agv2_asspos = buffer.lookupTransform(agv_pos.str(), ass_pos.str(), ros::Time(0));
                 geometry_msgs::Twist vel_msg;
                 double angular = agv2_asspos.transform.rotation.z;
-
+                ROS_WARN_STREAM(agv2_asspos);
                 double vel = 1 * sqrt(pow(agv2_asspos.transform.translation.x, 2) + pow(agv2_asspos.transform.translation.y, 2));
                 vel_msg.linear.x = vel;
                 if (vel < 0.04 && angular < 0.01)
@@ -161,19 +166,39 @@ int main(int argc, char *argv[])
                     vel_msg.linear.x = 0;
                     vel_msg.linear.y = 0;
                     vel_msg.angular.z = 0;
+                    ass_pos_count++;
                 }
                 else
                 {
-                    vel_msg.linear.x = 5 * agv2_asspos.transform.translation.x;
-                    vel_msg.linear.y = 5 * agv2_asspos.transform.translation.y;
-                    vel_msg.angular.z = 8 * agv2_asspos.transform.rotation.z;
+                    vel_msg.linear.x = 3 * agv2_asspos.transform.translation.x;
+                    vel_msg.linear.y = 3 * agv2_asspos.transform.translation.y;
+                    vel_msg.angular.z = 6 * agv2_asspos.transform.rotation.z;
                 }
-                agv_1_vel.publish(vel_msg);
+                agv_vel.publish(vel_msg);
+                topic_msg.data = assamble_status;
+                move_status.publish(topic_msg);
+            }
+            catch (const std::exception &e)
+            {
+                ROS_ERROR_STREAM("迅速对齐失败" << e.what() << '\n');
+                ros::Duration(0.5).sleep();
             }
         }
-        catch (const std::exception &r)
+        if (assamble_status == 1 && ass_pos_count >= 3)
         {
-            ROS_ERROR_STREAM(r.what());
+            geometry_msgs::Twist vel_msg;
+            vel_msg.linear.x = 0;
+            vel_msg.linear.y = 0;
+            vel_msg.angular.z = 0;
+            agv_vel.publish(vel_msg);
+            //锁定成功
+            assamble_status=2;
+            topic_msg.data = assamble_status;
+            move_status.publish(topic_msg);
+        }
+        // 锁定后，坐标变换啥的
+        if (assamble_status == 2)
+        {
         }
         rate.sleep();
     }
